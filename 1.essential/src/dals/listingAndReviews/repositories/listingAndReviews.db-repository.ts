9e797bddb8ffa3a8479cda73dc0b096309e9c77e
monicacrespo <moni.crespo@gmail.com>
2022-11-07 09:22:55 +0000

    Added mongoose feature

--- 1.essential/src/dals/listingAndReviews/repositories/listingAndReviews.db-repository.ts
+++ 1.essential/src/dals/listingAndReviews/repositories/listingAndReviews.db-repository.ts
@@ -1,24 +1,26 @@
 import { ListingAndReviewsRepository } from './listingAndReviews.repository';
 import { Review } from '../listingAndReviews.model';
-import { getListingAndReviewsContext } from '../listingAndReviews.context';
+import { listingAndReviewsContext } from '../listingAndReviews.context';
 
+// Due to Mongoose models provide several static helper functions, each of these functions returns mongoose query objects.
+// Using lean method to get a better performance.
 export const dbRepository: ListingAndReviewsRepository = {
   getListingAndReviewsList: async (country?: string, page?: number, pageSize?: number) => {
     const skip = Boolean(page) ? (page - 1) * pageSize : 0;
     const limit = Boolean(pageSize) ? pageSize : 0;  
     var query  = {};
     if (country !== 'undefined') query = {"address.country": country}
-    return await getListingAndReviewsContext()
+    return await listingAndReviewsContext
       .find(query)
       .skip(skip)
       .limit(limit)
-      .toArray();
+      .lean();
   },
   getListingAndReviews: async (id: string) => {
-    return await getListingAndReviewsContext().findOne({_id: id})
+    return await listingAndReviewsContext.findOne({_id: id}).lean();
   },
   insertReview: async (review: Review) => {
-    const { } = await getListingAndReviewsContext().findOneAndUpdate(
+   return await listingAndReviewsContext.findOneAndUpdate(
       {
         _id: review.listing_id,
         "reviews.reviewer_name":  { $ne: review.reviewer_name }
@@ -27,7 +29,6 @@ export const dbRepository: ListingAndReviewsRepository = {
       // If the value already exists in the array, $push will still append the value (resulting in duplicate values). 
       // However, $addToSet only appends the value if it doesn't already exist in the array. 
       // Therefore, if the value already exists, $addToSet won't append it (it will do nothing).
-    );
-    return review;
+    ).lean();
   }
 }
