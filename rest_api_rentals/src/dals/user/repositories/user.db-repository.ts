3d2128fb01206f85fd7c79ee2503ef211484093c
monicacrespo <moni.crespo@gmail.com>
2022-11-17 16:06:16 +0000

    Added mongoose to all endpoints

--- rest_api_rentals/src/dals/user/repositories/user.db-repository.ts
+++ rest_api_rentals/src/dals/user/repositories/user.db-repository.ts
@@ -1,11 +1,11 @@
 import { hashPassword } from 'common/helpers';
-import { getUserContext } from '../user.context';
+import { userContext } from '../user.context';
 import { User } from '../user.model';
 import { UserRepository } from './user.repository';
 
 export const dbRepository: UserRepository = {
   getUserByEmailAndPassword: async (email: string, password: string) => {
-    const user = await getUserContext().findOne({ email })
+    const user = await userContext.findOne({ email }).lean();
 
     const hashedPassword = await hashPassword(password, user?.salt);
     return user?.password === hashedPassword
--- rest_api_rentals/src/dals/user/user.context.ts
+++ rest_api_rentals/src/dals/user/user.context.ts
@@ -1,4 +1,12 @@
-import { db } from 'core/servers';
+import { model, Schema } from 'mongoose';
 import { User } from './user.model';
 
-export const getUserContext = () => db?.collection<User>('users');
+const userSchema = new Schema<User>({
+    _id: { type: Schema.Types.String, required: true },
+    email: { type: Schema.Types.String, required: true },
+    password: { type: Schema.Types.String, required: true },
+    salt: { type: Schema.Types.String, required: true },
+    role: { type: Schema.Types.String, required: true }
+})
+
+export const userContext = model<User>('user', userSchema);
